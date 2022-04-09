faec5c0c10acdd4e3bf1cb25b4f726a52811551b
greyspectrum <hello@greyspectrum.io>
2022-04-09 14:55:23 -0400

    function parameters

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
-// function definitions are themselves statements
-fn five() -> i32 {
-    5
+// Functions take parameters, the values
+// these parameters correspond to are called
+// arguments
+fn intiger(x: i32) {
+    println!("The value of x is: {}", x);
 }
 
 fn main() {
-    // a statement performs some action without 
-    // returning a value
-    let _y = 6;
-
-    // an expression evaluates to some value;
-    // ex. calling a function or macro, the value
-    // assigned in a statement, the {} braces
-    let _y = {
-        let x = 3;
-        x + 1 // note the lack of semicolon; 
-              // adding one turns it into a statement,
-              // which means it will no longer return a value
-    };
-   
-
-    let x = five();
-
-    println!("The value of x is {}", x);
+    intiger(5);
 }
