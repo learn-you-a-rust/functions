7cdb17ab1ed4783745344667d1d78bf2cd98bfab
greyspectrum <hello@greyspectrum.io>
2022-04-09 16:37:30 -0400

    functions with return values

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ fn intiger(x: i32) {
     println!("The value of x is: {}", x);
 }
 
+// Functions with return values should have
+// their type declared
+fn five() -> i32 {
+    5
+}
+
 fn main() {
     intiger(5);
+
+    let x = five();
+
+    println!("The value of x is: {}", x);
 }
